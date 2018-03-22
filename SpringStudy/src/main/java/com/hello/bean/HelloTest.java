58926d89ae56147702394d790804b9939a3b6fd7
wutao-boy <37316169+wutao-boy@users.noreply.github.com>
2018-03-22 23:04:06 +0800

    引入外部配置文件
    引入外部配置文件

--- SpringStudy/src/main/java/com/hello/bean/HelloTest.java
+++ SpringStudy/src/main/java/com/hello/bean/HelloTest.java
@@ -3,13 +3,28 @@ package com.hello.bean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class HelloTest {
 
-	
+	/**
+	 * helloword:
+	 * 1.使用context:component-scan标签进行包扫描
+	 * 2.在类上添加@Controller、@Service、@Repository，@Component注解
+	 * 
+	 * 引入外部配置文件：
+	 * 1.使用context:property-placeholder标签
+	 * 2.使用${var}填写值
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:beans-hello.xml");
+		//测试包扫描，helloword
 		Hello bean = ac.getBean(Hello.class);
-		String say = bean.say("搞事情");
+		String say = bean.say("我爱你北京");
 		System.out.println(say);
+		//测试外部引入文件
+		ComboPooledDataSource dataSource = (ComboPooledDataSource) ac.getBean("dataSource");
+		System.out.println(dataSource.toString());
 	}
 }
