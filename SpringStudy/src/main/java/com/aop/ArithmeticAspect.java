05dde0fb168fdddba7230229110081ca07e9c1c4
wutao-boy <37316169+wutao-boy@users.noreply.github.com>
2018-03-25 16:34:32 +0800

    aop

--- SpringStudy/src/main/java/com/aop/ArithmeticAspect.java
+++ SpringStudy/src/main/java/com/aop/ArithmeticAspect.java
@@ -11,15 +11,21 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 //需要把这个类声明为切面：1.需要把该类放到IOC容器中 2.再声明为一个切面
 @Aspect
 @Component
 public class ArithmeticAspect {
+	
+	//定义一个方法用于切入点表达式
+	//后面的其他通知，直接使用方法名来引入当前切入点表达式
+	@Pointcut("execution(public int com.aop.Arithmetic.*(..))")
+	public void declareJoinPoint(){}
 
 	//声明该方法是一个前置通知：在目标方法执行之前执行
-	@Before("execution(public int com.aop.Arithmetic.*(..))")
+	@Before("declareJoinPoint()")
 	public void beforeMethod(JoinPoint joinPoint){
 		String name = joinPoint.getSignature().getName();
 		List<Object> args = Arrays.asList(joinPoint.getArgs());
@@ -28,7 +34,7 @@ public class ArithmeticAspect {
 	
 	//后置通知：在目标方法执行后（无论是否发生异常），执行的通知
 	//在后置通知中还不能访问目标方法执行的结果
-	@After("execution(public int com.aop.Arithmetic.*(..))")
+	@After("declareJoinPoint()")
 	public void afterMethod(JoinPoint joinPoint){
 		String name = joinPoint.getSignature().getName();
 		System.out.println("after methodName:"+name);
@@ -36,7 +42,7 @@ public class ArithmeticAspect {
 	
 	//返回通知
 	//正常返回通知，可以访问到目标返回值
-	@AfterReturning(value="execution(public * com.aop.Arithmetic.*(..))",
+	@AfterReturning(value="declareJoinPoint()",
 			returning="result")
 	public void afterrunningMethod(JoinPoint joinPoint,Object result){
 		String name = joinPoint.getSignature().getName();
@@ -44,7 +50,7 @@ public class ArithmeticAspect {
 	}
 	
 	//在目标方法出现异常时会出现
-	@AfterThrowing(value="execution(public * com.aop.Arithmetic.*(..))",
+	@AfterThrowing(value="declareJoinPoint()",
 			throwing="ex")
 	public void afterThrowingMethod(JoinPoint joinPoint,Exception ex){
 		String name = joinPoint.getSignature().getName();
